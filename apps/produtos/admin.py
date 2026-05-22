0777ce675823feed1f47c79de300e42be1023c76
Mateus <mateusxmendes@gmail.com>
2026-05-22 16:52:29 -0300

    feat: listagem de produtos com dados reais do banco

--- apps/produtos/admin.py
+++ apps/produtos/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Categoria, Produto
 
-# Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'icone', 'ativo']
+    prepopulated_fields = {'slug': ('nome',)}
+    list_editable = ['ativo']
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'categoria', 'preco', 'estoque', 'ativo', 'destaque']
+    prepopulated_fields = {'slug': ('nome',)}
+    list_editable = ['ativo', 'destaque', 'preco', 'estoque']
+    list_filter = ['categoria', 'ativo', 'destaque']
+    search_fields = ['nome', 'descricao']
