ac0069fafb2715bca13b56e52cdc9f98f8aa22f0
Mateus <mateusxmendes@gmail.com>
2026-06-03 13:42:06 -0300

    feat: integração Cloudinary para armazenamento de imagens

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,12 @@ EMAIL_TIMEOUT = 10
 
 # Resend
 RESEND_API_KEY = os.getenv('RESEND_API_KEY')
+
+# Cloudinary
+import cloudinary
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
