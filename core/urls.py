1169d2dda3fc3e095f154b96641a8c207173683d
Mateus <mateusxmendes@gmail.com>
2026-06-03 12:31:22 -0300

    feat: SEO com meta tags, sitemap e robots.txt

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from apps.produtos.sitemaps import ProdutoSitemap, StaticSitemap
+
+sitemaps = {
+    'produtos': ProdutoSitemap,
+    'static': StaticSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +17,8 @@ urlpatterns = [
     path('', include('apps.pedidos.urls')),
     path('', include('apps.dashboard.urls')),
     path('', include('apps.pagamentos.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
