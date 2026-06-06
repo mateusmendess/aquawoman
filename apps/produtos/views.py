0b4ac4ea9d59f62e0b02d540405d6c61f2bf6797
Mateus <mateusxmendes@gmail.com>
2026-06-06 13:34:55 -0300

    feat: produtos agrupados por categoria com destaques primeiro

--- apps/produtos/views.py
+++ apps/produtos/views.py
@@ -4,22 +4,32 @@ from apps.pedidos.carrinho import Carrinho
 
 def lista_produtos(request):
     categorias = Categoria.objects.filter(ativo=True)
-    produtos = Produto.objects.filter(ativo=True)
-
     categoria_slug = request.GET.get('categoria')
     busca = request.GET.get('busca', '').strip()
 
     if categoria_slug:
-        produtos = produtos.filter(categoria__slug=categoria_slug)
-
-    if busca:
-        produtos = produtos.filter(nome__icontains=busca)
+        produtos = Produto.objects.filter(ativo=True, categoria__slug=categoria_slug).order_by('-destaque', 'nome')
+        categorias_com_produtos = None
+    elif busca:
+        produtos = Produto.objects.filter(ativo=True, nome__icontains=busca).order_by('-destaque', 'nome')
+        categorias_com_produtos = None
+    else:
+        produtos = None
+        categorias_com_produtos = []
+        for categoria in categorias:
+            prods = Produto.objects.filter(ativo=True, categoria=categoria).order_by('-destaque', 'nome')
+            if prods.exists():
+                categorias_com_produtos.append({
+                    'categoria': categoria,
+                    'produtos': prods,
+                })
 
     context = {
         'produtos': produtos,
         'categorias': categorias,
         'categoria_slug': categoria_slug,
         'busca': busca,
+        'categorias_com_produtos': categorias_com_produtos,
     }
 
     if request.headers.get('HX-Request'):
