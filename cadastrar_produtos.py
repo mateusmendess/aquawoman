3c374dbb96befb11b8b0fdc4d7a99d91ea27b745
Mateus Mendes <mateusxmendes@gmail.com>
2026-06-27 16:50:30 -0300

    feat: cadastro de produtos e categorias reais da Aquawoman

---
+++ cadastrar_produtos.py
@@ -0,0 +1,246 @@
+# -*- coding: utf-8 -*-
+import os, django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+from apps.pedidos.models import ItemPedido, Pedido
+from apps.produtos.models import Categoria, Produto
+from django.utils.text import slugify
+
+print("Removendo itens de pedidos...")
+ItemPedido.objects.all().delete()
+print("Removendo pedidos...")
+Pedido.objects.all().delete()
+print("Removendo produtos...")
+Produto.objects.all().delete()
+print("Removendo categorias...")
+Categoria.objects.all().delete()
+print("Removido!")
+
+print("Criando categorias...")
+categorias_data = [
+    ("Agua Sanitaria",            "agua-sanitaria"),
+    ("Desinfetante",              "desinfetante"),
+    ("Detergente",                "detergente"),
+    ("Papel Higienico",           "papel-higienico"),
+    ("Papel Toalha",              "papel-toalha"),
+    ("Pano",                      "pano"),
+    ("Saco para Lixo",            "saco-para-lixo"),
+    ("Sabao",                     "sabao"),
+    ("Copos Descartaveis",        "copos-descartaveis"),
+    ("Luvas",                     "luvas"),
+    ("Vassoura e Rodo",           "vassoura-rodo"),
+    ("Limpa Aluminio e Multiuso", "limpa-aluminio-multiuso"),
+    ("Desodorizador Bom Ar",      "desodorizador-bom-ar"),
+    ("Esponja",                   "esponja"),
+    ("Alcool",                    "alcool"),
+    ("Inseticida",                "inseticida"),
+    ("Diversos",                  "diversos"),
+]
+
+categorias = {}
+for nome, slug in categorias_data:
+    c = Categoria.objects.create(nome=nome, slug=slug, icone="", ativo=True)
+    categorias[nome] = c
+    print("  OK: " + nome)
+
+print("\nCadastrando produtos...")
+produtos_data = [
+    ("Agua Sanitaria Economica 1L", "Agua Sanitaria", 2.81, "Embalagem 1L"),
+    ("Agua Sanitaria Economica 2L", "Agua Sanitaria", 5.33, "Embalagem 2L"),
+    ("Agua Sanitaria Economica 5L", "Agua Sanitaria", 14.51, "Embalagem 5L"),
+    ("Agua Sanitaria FC Verde 1L", "Agua Sanitaria", 2.16, "Embalagem 1L"),
+    ("Agua Sanitaria FC Verde 2L", "Agua Sanitaria", 3.65, "Embalagem 2L"),
+    ("Agua Sanitaria FC Verde 5L", "Agua Sanitaria", 10.47, "Embalagem 5L"),
+    ("Desinfetante Floral 1L", "Desinfetante", 4.28, "Embalagem 1L"),
+    ("Desinfetante Floral 2L", "Desinfetante", 8.24, "Embalagem 2L"),
+    ("Desinfetante Floral 5L", "Desinfetante", 20.87, "Embalagem 5L"),
+    ("Desinfetante Jasmim 1L", "Desinfetante", 4.28, "Embalagem 1L"),
+    ("Desinfetante Jasmim 2L", "Desinfetante", 8.24, "Embalagem 2L"),
+    ("Desinfetante Lavanda 1L", "Desinfetante", 4.28, "Embalagem 1L"),
+    ("Desinfetante Lavanda 2L", "Desinfetante", 8.24, "Embalagem 2L"),
+    ("Desinfetante Lavanda 5L", "Desinfetante", 20.87, "Embalagem 5L"),
+    ("Desinfetante Pinho Sol 3,8L", "Desinfetante", 54.55, "Embalagem 3,8L"),
+    ("Desinfetante Azulim 500ML", "Desinfetante", 3.93, "Embalagem 500ML"),
+    ("Desinfetante Azulim Lavanda 1L", "Desinfetante", 0.00, "Embalagem 1L"),
+    ("Desinfetante Azulim Wave 1L", "Desinfetante", 0.00, "Embalagem 1L"),
+    ("Lava Louca Economico Limao 500ML", "Detergente", 2.58, "Embalagem 500ML"),
+    ("Lava Louca Economico Maca 500ML", "Detergente", 2.58, "Embalagem 500ML"),
+    ("Lava Louca Economico Neutro 500ML", "Detergente", 2.58, "Embalagem 500ML"),
+    ("Lava Louca FC Limao Pet 50ML", "Detergente", 1.98, "Embalagem 50ML"),
+    ("Lava Louca FC Maca Pet 500ML", "Detergente", 1.98, "Embalagem 500ML"),
+    ("Lava Louca FC Neutro Pet 5000ML", "Detergente", 1.98, "Embalagem 5000ML"),
+    ("Lava Louca OI Limao 500ML", "Detergente", 1.73, "Embalagem 500ML"),
+    ("Lava Louca OI Maca 500ML", "Detergente", 1.73, "Embalagem 500ML"),
+    ("Lava Louca OI Neutro 500ML", "Detergente", 1.73, "Embalagem 500ML"),
+    ("Lava Louca OI Neutro 5L", "Detergente", 19.13, "Embalagem 5L"),
+    ("Lava Louca Economico 5L", "Detergente", 24.75, "Embalagem 5L"),
+    ("Lava Louca Azulim 500ML", "Detergente", 0.00, "Embalagem 500ML"),
+    ("Papel Higienico 30M Mili 16X4 Neutro", "Papel Higienico", 18.70, "PCT C/16"),
+    ("Papel Hig. Elegans Duo 8 Rolos 50M", "Papel Higienico", 136.58, "8 Rolos/50M"),
+    ("Papel Hig. Elegans Soft Simples 8 Rolos 300M", "Papel Higienico", 69.76, "8 Rolos/300M"),
+    ("Papel Hig. Mili Neutro Bianco 30M", "Papel Higienico", 4.68, "Unitario"),
+    ("Papel Hig. Atualle 20M 8X12", "Papel Higienico", 19.39, "16x4"),
+    ("Papel Hig. Mili Neutro CMPC 4X16", "Papel Higienico", 12.84, "4x30M"),
+    ("Papel Hig. Cai Cai FL Duplo Elegans", "Papel Higienico", 129.20, "16/500FLS"),
+    ("Papel Hig. Equilibrio 300M", "Papel Higienico", 78.68, "300M"),
+    ("Papel Toalha Interfolhas Unique 1000FL 19,4/20CM", "Papel Toalha", 19.72, "1000FL 19,4/20CM"),
+    ("Papel Toalha Interfolhas Unique 1000FL 23/20CM", "Papel Toalha", 23.52, "1000FL 23/20CM"),
+    ("Papel Toalha Bobina 3 Rolos 360 Folhas", "Papel Toalha", 19.61, "3 Rolos 360 Folhas"),
+    ("Papel Toalha Bobina Tenaz 200M 6 Rolos", "Papel Toalha", 109.71, "200M/6 Rolos"),
+    ("Papel Toalha Bobina 100% Celulose Fit Alveflor 200M", "Papel Toalha", 124.99, "200M/6 Rolos"),
+    ("Papel Toalha Bobina 100% Celulose 200M", "Papel Toalha", 77.99, "200M/6 Rolos"),
+    ("Papel Toalha Bobina Equilibrio 200M", "Papel Toalha", 80.41, "200M/6 Rolos"),
+    ("Papel Toalha Interfolha Equilibrio 1000FL", "Papel Toalha", 17.13, "1000FL/19,4/20CM"),
+    ("Papel Toalha Snob 3 Unidades", "Papel Toalha", 19.61, "3 Unidades"),
+    ("Papel Aluminio 30X7,5MT", "Papel Toalha", 5.85, "30X7,5MT"),
+    ("Pano Microfibra Polylar 35CMX50CM", "Pano", 6.75, "Unitario"),
+    ("Pano Multiuso Polylar Branco 28CMX50CM", "Pano", 138.99, "300M"),
+    ("Pano Multiuso Rolo Azul 25M", "Pano", 21.87, "25M/50 Panos"),
+    ("Pano de Microfibra 100X100", "Pano", 22.47, "100/100"),
+    ("Pano Multiuso Rolo Azul 240M", "Pano", 93.80, "240M/600 Panos"),
+    ("Pano Alvejado Copalimpa 70X45CM", "Pano", 10.50, "70X45CM"),
+    ("Pano Alvejado RA Novo Mundo 40X64CM", "Pano", 5.07, "40X64CM"),
+    ("Pano Xadrez Polylar 60X80CM", "Pano", 12.50, "60X80CM"),
+    ("Pano Cru Novo Mundo 58X80CM", "Pano", 5.85, "50X80CM"),
+    ("Saco de Lixo Azul 15L Rolo 60 Unidades", "Saco para Lixo", 19.30, "Rolo 60 Uni"),
+    ("Saco de Lixo Azulim 30L Rolo 30 Unidades", "Saco para Lixo", 0.00, "Rolo 30 Uni"),
+    ("Saco de Lixo Azulim 50L Rolo 30 Unidades", "Saco para Lixo", 0.00, "Rolo 30 Uni"),
+    ("Saco de Lixo Vorel 100L 15 Unidades", "Saco para Lixo", 0.00, "PCT 15 Uni"),
+    ("Saco Lixo Azulim PCT 20 Unidades", "Saco para Lixo", 5.75, "PCT 20 Uni"),
+    ("Saco Lixo Preto Sacolmax 30LT PCT 10 Unidades", "Saco para Lixo", 3.99, "PCT 10 Uni"),
+    ("Saco Lixo Preto Sacolmax 50LT PCT 10 Unidades", "Saco para Lixo", 4.80, "PCT 10 Uni"),
+    ("Saco Lixo Preto Sacolmax 100LT PCT 10 Unidades", "Saco para Lixo", 5.20, "PCT 10 Uni"),
+    ("Saco de Lixo 30L Rolo 50 Unidades", "Saco para Lixo", 17.20, "Rolo 50 Uni"),
+    ("Saco de Lixo 50L Rolo 40 Unidades", "Saco para Lixo", 17.20, "Rolo 40 Uni"),
+    ("Saco de Lixo 100L Rolo 20 Unidades", "Saco para Lixo", 17.20, "Rolo 20 Uni"),
+    ("Saco de Lixo Leve 100L PCT 100 Unidades", "Saco para Lixo", 49.50, "PCT 100 Uni"),
+    ("Saco de Lixo Medio 100L PCT 100 Unidades", "Saco para Lixo", 61.80, "PCT 100 Uni"),
+    ("Saco de Lixo Reforcado 100L PCT 100 Unidades", "Saco para Lixo", 81.80, "PCT 100 Uni"),
+    ("Saco de Lixo Medio 200L PCT 100 Unidades", "Saco para Lixo", 95.80, "PCT 100 Uni"),
+    ("Saco de Lixo Reforcado 200L PCT 100 Unidades", "Saco para Lixo", 126.00, "PCT 100 Uni"),
+    ("Lava Roupa em Barra PCT 5 Unidades", "Sabao", 9.50, "PCT 5 Uni"),
+    ("Lava Roupa Liquido OI 5L", "Sabao", 29.35, "Embalagem 5L"),
+    ("Lava Roupa Liquido ALA Lavanda 1,8L", "Sabao", 16.50, "Embalagem 1,8L"),
+    ("Lava Roupa Liquido ALA 3L", "Sabao", 25.60, "Embalagem 3L"),
+    ("Lava Roupa em Po ALA Lavanda 400G", "Sabao", 4.07, "Embalagem 400G"),
+    ("Lava Roupa em Po Brilhante 400G", "Sabao", 6.36, "Embalagem 400G"),
+    ("Lava Roupa em Po Classico Tradicional 4KG", "Sabao", 28.79, "Embalagem 4KG"),
+    ("Lava Roupa em Po Guarany 4KG", "Sabao", 21.40, "Embalagem 4KG"),
+    ("Lava Roupa Po Tixan Ype 800G", "Sabao", 10.54, "Embalagem 800G"),
+    ("Lava Roupa Po Tixan Ype 1,3KG", "Sabao", 17.04, "Embalagem 1,3KG"),
+    ("Lava Roupa Po Tixan Ype 4KG", "Sabao", 48.99, "Embalagem 4KG"),
+    ("Lava Roupa Tixan Ipe 8KG", "Sabao", 97.66, "Embalagem 8KG"),
+    ("Lava Roupa Tixan Ipe 400G", "Sabao", 5.66, "Embalagem 400G"),
+    ("Lava Roupa Liquido Tuff 1,5L", "Sabao", 18.71, "Embalagem 1,5L"),
+    ("Lava Roupa Liquido Economico 3L", "Sabao", 16.83, "Embalagem 3L"),
+    ("Copo Descartavel Cristcopo 180ML CX 25 Pacotes", "Copos Descartaveis", 5.42, "CX 25 Pacotes 100 Uni"),
+    ("Copo Descartavel Cristcopo 200ML CX 25 Pacotes", "Copos Descartaveis", 6.42, "CX 25 Pacotes 100 Uni"),
+    ("Copo Descartavel Cristcopo 50ML CX 50 Pacotes", "Copos Descartaveis", 3.22, "CX 50 Pacotes 100 Uni"),
+    ("Copo BR 150ML CX 25 Pacotes", "Copos Descartaveis", 4.39, "CX 25 Pacotes 100 Uni"),
+    ("Copo BR 180ML CX 25 Pacotes", "Copos Descartaveis", 4.79, "CX 25 Pacotes 100 Uni"),
+    ("Copo BR 200ML CX 25 Pacotes", "Copos Descartaveis", 5.59, "CX 25 Pacotes 100 Uni"),
+    ("Copo BR 300ML CX 20 Pacotes", "Copos Descartaveis", 9.24, "CX 20 Pacotes 100 Uni"),
+    ("Luva Vabene Multiuso Flex M/G Par", "Luvas", 5.72, "Par"),
+    ("Luva Latex Nobre Caixa", "Luvas", 32.50, "Caixa"),
+    ("Luva Nitrilica Preta Caixa", "Luvas", 35.70, "Caixa"),
+    ("Luva Mucambo Amarela Par", "Luvas", 9.28, "Par"),
+    ("Luva Ranhurada M Par", "Luvas", 18.62, "Par"),
+    ("Luva de Borracha G Par", "Luvas", 7.50, "Par"),
+    ("Vassoura Versalli com Cabo Faxina", "Vassoura e Rodo", 14.09, "Unitario"),
+    ("Vassoura Norcia com Cabo Faxina", "Vassoura e Rodo", 15.16, "Unitario"),
+    ("Rodo com Cabo de Madeira 40CM", "Vassoura e Rodo", 14.69, "40CM"),
+    ("Rodo Aluminio Start 40CM", "Vassoura e Rodo", 41.24, "40CM"),
+    ("Rodo Aluminio Start 60CM", "Vassoura e Rodo", 49.38, "60CM"),
+    ("Rodo Aluminio Start 80CM", "Vassoura e Rodo", 57.40, "80CM"),
+    ("Limpa Aluminio Economico 500ML", "Limpa Aluminio e Multiuso", 1.95, "Embalagem 500ML"),
+    ("Limpa Aluminio Azulim 500ML", "Limpa Aluminio e Multiuso", 0.00, "Embalagem 500ML"),
+    ("Limpa Forno 500ML", "Limpa Aluminio e Multiuso", 18.75, "Embalagem 500ML"),
+    ("Limpa Aluminio Politriz 500ML", "Limpa Aluminio e Multiuso", 4.26, "Embalagem 500ML"),
+    ("Limpador Perfumado Azulim 1L", "Limpa Aluminio e Multiuso", 8.11, "Embalagem 1L"),
+    ("Multiuso Azulim 500ML", "Limpa Aluminio e Multiuso", 0.00, "Embalagem 500ML"),
+    ("Multi-Uso Cremoso 300ML", "Limpa Aluminio e Multiuso", 10.20, "Embalagem 300ML"),
+    ("Multi-Uso Cha Branco 500ML", "Limpa Aluminio e Multiuso", 5.12, "Embalagem 500ML"),
+    ("Multi-Uso Economico 500ML", "Limpa Aluminio e Multiuso", 4.60, "Embalagem 500ML"),
+    ("Multi-Uso Veja Uso Original 500ML", "Limpa Aluminio e Multiuso", 5.33, "Embalagem 500ML"),
+    ("Bom Ar Alegria 360ML", "Desodorizador Bom Ar", 13.98, "Embalagem 360ML"),
+    ("Bom Ar Jasmim 360ML", "Desodorizador Bom Ar", 13.98, "Embalagem 360ML"),
+    ("Bom Ar Limao 360ML", "Desodorizador Bom Ar", 13.98, "Embalagem 360ML"),
+    ("Bom Ar Romance 360ML", "Desodorizador Bom Ar", 13.98, "Embalagem 360ML"),
+    ("Bom Ar Talco 360ML", "Desodorizador Bom Ar", 13.98, "Embalagem 360ML"),
+    ("Esponja Pertuto Multiuso", "Esponja", 1.90, "Unitario"),
+    ("La de Aco 6 Unidades", "Esponja", 2.30, "6 Uni"),
+    ("Alcool Gel Antisseptico 70% 1L", "Alcool", 9.38, "Embalagem 1L"),
+    ("Alcool 70% Azulim 1L", "Alcool", 11.50, "Embalagem 1L"),
+    ("Alcool Hidratado Liquido 46 INPM 1L", "Alcool", 10.30, "Embalagem 1L"),
+    ("Alcool Hidratado 70 INPM 1L", "Alcool", 12.75, "Embalagem 1L"),
+    ("Alcool Liquido 70% Start 1L", "Alcool", 11.50, "Embalagem 1L"),
+    ("Alcool 46 Bactericida Top 5L", "Alcool", 54.90, "Embalagem 5L"),
+    ("Alcool 70% Asseotgel 5L", "Alcool", 55.95, "Embalagem 5L"),
+    ("Alcool Gel para Maos 420G", "Alcool", 12.99, "Embalagem 420G"),
+    ("Inseticida SPB", "Inseticida", 16.93, "Unitario"),
+    ("Inseticida Baygom", "Inseticida", 16.00, "Unitario"),
+    ("Lustra Moveis Azulim 200G", "Diversos", 7.20, "Embalagem 200G"),
+    ("Sabonete Liquido Erva Doce 5L", "Diversos", 52.24, "Embalagem 5L"),
+    ("Sabonete Bouquet Verde 5L", "Diversos", 60.96, "Embalagem 5L"),
+    ("Sabonete Erva Doce com Hortela 500ML", "Diversos", 13.58, "Embalagem 500ML"),
+    ("Sabonete Only Verde Perolado 5L", "Diversos", 52.65, "Embalagem 5L"),
+    ("Pulverizador Spray 1L", "Diversos", 16.58, "Embalagem 1L"),
+    ("Pulverizador Manual Vazio 500ML", "Diversos", 5.47, "Frasco 500ML"),
+    ("Coador Flanela Aro Metal N08", "Diversos", 4.62, "Unitario"),
+    ("Pastilhas Sanitaria CX 3 Unidades", "Diversos", 7.87, "CX 3 Uni"),
+    ("Pedra Sanitaria", "Diversos", 2.20, "Unitario"),
+    ("Flanela Copa Lima Laranja Branca 30X50", "Diversos", 4.70, "30X50CM"),
+    ("Amaciante Tuff Cha Branco 1,5L", "Diversos", 22.26, "Embalagem 1,5L"),
+    ("Amaciante Soft Blue 5L", "Diversos", 23.50, "Embalagem 5L"),
+    ("Argus Detergente 5L", "Diversos", 64.68, "Embalagem 5L"),
+    ("Hipoclorito de Sodio 5L", "Diversos", 51.20, "Embalagem 5L"),
+    ("Magico Removedor de Ceras 5L", "Diversos", 62.50, "Embalagem 5L"),
+    ("Naftalina Nobre 50G", "Diversos", 3.60, "Embalagem 50G"),
+    ("Naftalina Azulim 30G", "Diversos", 3.29, "Embalagem 30G"),
+    ("Pedrex 5L", "Diversos", 53.59, "Embalagem 5L"),
+    ("Percabonato 1KG", "Diversos", 35.00, "Embalagem 1KG"),
+    ("Peroxi Lavanda 5L", "Diversos", 155.00, "Embalagem 5L"),
+    ("Pretita 5L", "Diversos", 107.95, "Embalagem 5L"),
+    ("Alvejante Economico 1L", "Diversos", 6.71, "Embalagem 1L"),
+    ("Alvejante Economico 2L", "Diversos", 12.26, "Embalagem 2L"),
+    ("Cabo de Aluminio 1,5M", "Diversos", 34.25, "Unitario"),
+    ("Escova para Lavar Roupa", "Diversos", 3.33, "Unitario"),
+    ("Tira Ferrugem Azulim 50ML", "Diversos", 0.00, "Embalagem 50ML"),
+    ("Toca Rede Nylon 100 Unidades", "Diversos", 32.63, "PCT 100 Uni"),
+    ("Toca Descartavel", "Diversos", 10.34, "Unitario"),
+    ("Guardanapo", "Diversos", 2.50, "Unitario"),
+]
+
+slugs_usados = set()
+criados = 0
+erros = 0
+
+for nome, cat_nome, preco, descricao in produtos_data:
+    try:
+        categoria = categorias[cat_nome]
+        slug_base = slugify(nome)
+        slug = slug_base
+        contador = 1
+        while slug in slugs_usados:
+            slug = slug_base + "-" + str(contador)
+            contador += 1
+        slugs_usados.add(slug)
+        Produto.objects.create(
+            nome=nome,
+            slug=slug,
+            descricao=descricao,
+            preco=preco,
+            estoque=0,
+            categoria=categoria,
+            ativo=True,
+            destaque=False,
+        )
+        criados += 1
+    except Exception as e:
+        print("ERRO em " + nome + ": " + str(e))
+        erros += 1
+
+print("\n" + str(criados) + " produtos cadastrados com sucesso!")
+if erros:
+    print(str(erros) + " erros")
+print("Feito!")
